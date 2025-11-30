2529cebd20b841dc358b19cabc8e982afcfd50b8
NavinVenkatV <vnavinvenkat@gmail.com>
2025-11-30 16:17:18 +0100

    next-intl-installation

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+import createNextIntlPlugin from 'next-intl/plugin';
+const withNextIntl = createNextIntlPlugin();
 const nextConfig = {
     images: {
       remotePatterns: [
@@ -14,5 +16,5 @@ const nextConfig = {
     },
   };
   
-  export default nextConfig;
+  export default withNextIntl(nextConfig);
   
